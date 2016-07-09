d2877f3498a6e05f35161525900924127f21c91e
ajinkyanashte <ajinkyanashte007@gmail.com>
2016-07-09 18:56:24 +0530

    adding new file

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+if(isset($_POST) && !empty($_POST)){
+	$userName = $_POST['username'];
+	$password = $_POST['password'];
+	//mysql query
+	if(isset($userName) && isset($password) && !empty($userName) && !empty($password)){
+		$_SESSION['username'] = $userName;
+	}else{
+		echo "please enter valid details";
+	}
+}
+
+if(isset($_SESSION['username']) && !empty($_SESSION['username'])){
+	echo "<label>LOGOUT</label>";
+}else{
+	echo "<label>LOGIN</label>"; 
+}
+?>
+
+
+<html>
+	<form method="post">
+		<input type="text" name ="username"/>
+		<br />
+		<input type="password" name ="password"/>
+		<br />
+		<input type="submit" name="submit" />
+		<br />
+		<input type="reset" name="RESET" />
+	</form>
+</html>
